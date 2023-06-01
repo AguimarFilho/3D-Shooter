590b27ab7d4817afd3ecc4ad9ec97843e0b8b3c1
Gio <aguimarfilho2308@gmail.com>
2023-06-01 16:41:19 -0300

    Shooter 3D 1.5.6
    Update: 1.5.6
    Corrigindo pulo, parte 1.

--- Shooter3D_UnityWindows/Assets/_Scripts/Player/PlayerLocomotion.cs
+++ Shooter3D_UnityWindows/Assets/_Scripts/Player/PlayerLocomotion.cs
@@ -13,13 +13,29 @@ public class PlayerLocomotion : MonoBehaviour
 
     public float _jumpForce;
 
-    [SerializeField] private float _gravityScale = -9.81f;
-    [SerializeField] private float _gravityMultiplier = 1f;
-
     [HideInInspector] public float _velocityY;
-    private Vector3 newDirection;
+    private Vector3 _newDirection;
+
+    [Header("Ground & Air Verification")]
+    [Space(15)]
+
+    [SerializeField] private float _groundDetectionRayStartPoint = 0.5f;
+    [SerializeField] private float _minimumDistanceToFall = 1f;
+    [SerializeField] private float _groundDirectionRayDistance = 0.2f;
+    [SerializeField] private LayerMask _ignorForGroundCheck;
+    private bool IsGrounded = false;
+    private bool IsInAir;
+
 
     private AnimationManager _animationManager;
+
+    [Header("Gravity Parameters")]
+
+    [SerializeField] private float _gravityScale = -9.81f;
+    [SerializeField] private float _gravityMultiplier = 1f;
+    private Vector3 _normalVector;
+    private Vector3 _targetPosition;
+    private float _isInAirTime;
 
     public bool _isJumpPressed;
 
@@ -28,47 +44,101 @@ public class PlayerLocomotion : MonoBehaviour
         _animationManager = FindObjectOfType<AnimationManager>();
 
 
-        _inputManager.GameControls.Player.Jump.performed += Jump;
-        _inputManager.GameControls.Player.Jump.canceled += Jump;
+        //_inputManager.GameControls.Player.Jump.performed += Jump;
+        //_inputManager.GameControls.Player.Jump.canceled += Jump;
     }
 
     private void Update()
     {
-        ApplyGravity();
+        ApplyGravity(_newDirection);
         Walk();
+    }
+    private void LateUpdate()
+    {
+        _isInAirTime = _isInAirTime + Time.deltaTime;
     }
 
     private void Walk()
     {
         Vector2 walkDirection = _inputManager.GameControls.Player.Locomotion.ReadValue<Vector2>();
 
-        newDirection = new Vector3(walkDirection.x, 0, walkDirection.y);
-        newDirection.y = _velocityY;
+        _newDirection = new Vector3(walkDirection.x, 0, walkDirection.y);
+        _newDirection.y = _velocityY;
 
-        _characterController.Move(newDirection * (_walkspeed * Time.deltaTime));
+        _characterController.Move(_newDirection * (_walkspeed * Time.deltaTime));
     }
 
-    private void ApplyGravity()
+    private void ApplyGravity(Vector3 moveDirection)
     {
-        if(_characterController.isGrounded && _velocityY < 0f)
+        _newDirection.y = _velocityY;
+
+        IsGrounded = false;
+        RaycastHit Hit;
+        Vector3 origin = transform.localPosition;
+        origin.y += _groundDetectionRayStartPoint;
+
+        if(Physics.Raycast(origin, transform.forward, out Hit, 0.4f))
         {
-            _velocityY = 0f;
+            _velocityY = -1f;
+        }
+        if(IsInAir)
+        {
+            _velocityY = _gravityScale * _gravityMultiplier;
+        }
+
+        Vector3 dir = moveDirection;
+        dir.Normalize();
+        origin = origin + dir * _groundDirectionRayDistance;
+
+        Debug.DrawRay(origin, -Vector3.up * _minimumDistanceToFall, Color.red, 0.1f, false);
+
+        if(Physics.Raycast(origin, -Vector3.up, out Hit, _minimumDistanceToFall, _ignorForGroundCheck))
+        {
+            _normalVector = Hit.normal;
+            Vector3 tp = Hit.point;
+            IsGrounded = true;
+            _targetPosition.y = tp.y;
+
+            if(IsInAir)
+            {
+                if(_isInAirTime > 0.5f)
+                {
+                    Debug.Log("To na transição da queda pro  chão");
+                    _isInAirTime = 0;
+                }
+                else
+                {
+                    Debug.Log("To no chão");
+                    _isInAirTime = 0;
+                }
+
+                IsInAir = false;
+            }
         }
         else
         {
-            _velocityY += _gravityScale * _gravityMultiplier * Time.deltaTime;
+            if(IsGrounded)
+            {
+                IsGrounded = false;
+            }
+            if(IsInAir == false)
+            {
+                Debug.Log("Caindo porra");
+
+                IsInAir = true;
+            }
         }
     }
 
-    private void Jump(InputAction.CallbackContext ctx)
+   private void Jump(InputAction.CallbackContext ctx)
     {
         _isJumpPressed = ctx.ReadValueAsButton();
         
-        if(_characterController.isGrounded && _isJumpPressed)
+        if(IsGrounded && _isJumpPressed)
         {
             newDirection.y += _jumpForce;
 
-            if (!_characterController.isGrounded)
+            if (!IsGrounded)
             {
                 newDirection.y = _velocityY;
             }
